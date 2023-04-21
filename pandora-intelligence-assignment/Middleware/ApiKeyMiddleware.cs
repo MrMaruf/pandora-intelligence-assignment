f166d1f2698fff157a1fd3477cdaad0c866e0d39
Maruf Abdirimov <abdirimov.maruf@gmail.com>
2023-04-21 17:56:16 +0200

    Slight visual fix

--- pandora-intelligence-assignment/Middleware/ApiKeyMiddleware.cs
+++ pandora-intelligence-assignment/Middleware/ApiKeyMiddleware.cs
@@ -3,8 +3,7 @@
     public class ApiKeyMiddleware
     {
         private readonly RequestDelegate _next;
-        private
-        const string APIKEY = "X-API-Key";
+        private const string APIKEY = "X-API-Key";
         public ApiKeyMiddleware(RequestDelegate next)
         {
             _next = next;
