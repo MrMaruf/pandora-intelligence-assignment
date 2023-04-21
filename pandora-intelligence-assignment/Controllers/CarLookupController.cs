de3508bcf0ee660ad6458b1f6aaf6f6bc6224680
Maruf Abdirimov <abdirimov.maruf@gmail.com>
2023-04-21 19:02:12 +0200

    Removed parts from project generation

--- pandora-intelligence-assignment/Controllers/CarLookupController.cs
+++ pandora-intelligence-assignment/Controllers/CarLookupController.cs
@@ -8,10 +8,6 @@ namespace pandora_intelligence_assignment.Controllers
     [Route("[controller]")]
     public class CarLookupController : ControllerBase
     {
-        private static readonly string[] Summaries = new[]
-        {
-        "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-    };
 
         private readonly ILogger<CarLookupController> _logger;
 
@@ -51,13 +47,6 @@ namespace pandora_intelligence_assignment.Controllers
                     return null;
                 }
             }
-            //return Enumerable.Range(1, 5).Select(index => new WeatherForecast
-            //{
-            //    Date = DateTime.Now.AddDays(index),
-            //    TemperatureC = Random.Shared.Next(-20, 55),
-            //    Summary = Summaries[Random.Shared.Next(Summaries.Length)]
-            //})
-            //.ToArray();
         }
     }
 }
